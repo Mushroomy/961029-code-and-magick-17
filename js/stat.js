45049b2957d6d36d555e289ae8f7720e4496dba3
Mushroomy <mushroomy.work@gmail.com>
2019-06-12 10:01:59 +0300

    Update stat.js

--- js/stat.js
+++ js/stat.js
@@ -9,13 +9,16 @@ var GAP = 50;
 var FONT_GAP = 20;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
+var COLOR_BLACK = '#000';
+var COLOR_WHITE = '#fff';
+var FONT_STYLE = '16px PT Mono';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
+  ctx.fillStyle = COLOR_BLACK;
+  ctx.font = FONT_STYLE;
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', 210, 30);
   ctx.fillText('Список результатов:', 210, 48);
@@ -34,9 +37,9 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.3)', 100);
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff', 100);
-  ctx.fillStyle = '#000';
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, COLOR_WHITE);
+  ctx.fillStyle = COLOR_BLACK;
   var maxTime = getMaxElement(times);
 
   function randomInteger(min, max) {
@@ -46,7 +49,7 @@ window.renderStatistics = function (ctx, names, times) {
   for (var i = 0; i < names.length; i++) {
     var roundTimes = Math.round(times[i]);
     var roundHeight = Math.round((BAR_HEIGHT * roundTimes) / maxTime);
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = COLOR_BLACK;
     ctx.fillText(names[i], CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - FONT_GAP);
     ctx.fillText(roundTimes, CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - CLOUD_Y - FONT_GAP - FONT_GAP - roundHeight);
     if (names[i] === 'Вы') {
